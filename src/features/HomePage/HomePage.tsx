8a85dffe26b029ffbb1c324cf165f4611da55796
Vyachaslau <sokol.v083@gmail.com>
2024-11-13 12:43:05 +0300

    fix: Add 3 typeps of focus on input
    second type - callback on ref, third type - atr autoFocus

--- src/features/HomePage/HomePage.tsx
+++ src/features/HomePage/HomePage.tsx
@@ -1,11 +1,22 @@
 import { Link } from 'react-router-dom'
 import styles from './HomePage.module.css'
 import { useAppDispatch, useAuth } from '../../app/hooks'
+import { useEffect, useRef, useState } from 'react'
 
 const HomePage = () => {
+  const [isVis, setIsVis] = useState(false)
+  const refInput = useRef()
   const isAuth = useAuth().isAuth
-  console.log(isAuth)
   const dispatch = useAppDispatch()
+
+  const toggle = () => {
+    setIsVis(!isVis);
+
+  }
+
+  // useEffect(() => {
+  //   if (isVis) refInput.current.focus()
+  // }, [isVis])
 
   return (
     <div  className={styles.homePage}>
@@ -13,7 +24,12 @@ const HomePage = () => {
         Home Page
         { !isAuth 
           ? <Link to="/register">Sign Up Page</Link> 
-          : <button onClick={()=>{}}>logout</button>}
+          : <button onClick={()=>{}}>logout</button>
+        }
+        <button onClick={toggle}>Show input and focus it</button>
+        {isVis && <input type='text' ref={refInput => refInput?.focus()}/>}
+        {/* {isVis && <input type='text' ref={refInput}/>} */}
+        {/* {isVis && <input type='text' autoFocus/>} */}
     </div>
   )
 }
