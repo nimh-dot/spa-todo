e76a6996a5176fd03e8fb91357fc2b24f81ff2db
Vyachaslau <sokol.v083@gmail.com>
2024-11-15 16:08:58 +0300

    fix: svg import
    1. replace img tag to ReactSVG in footer - social. 2. replace img tag in footer - logo to Logo component

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -1,6 +1,8 @@
 import styles from './footer.module.css'
 import { Link } from 'react-router-dom'
 import icon_telegram from './img/icon-telegram.svg'
+import Logo from '../../components/Logo/Logo'
+import { ReactSVG } from 'react-svg'
 
 
 const Footer = () => {
@@ -8,12 +10,12 @@ const Footer = () => {
     <div className={styles.footer}>
       <div className={styles.footer__logo}>
         <Link to="/">
-          <img src="logo.png" alt="logo"/>
+          <Logo />
         </Link>
       </div>
       <div className={styles.footer__social}>
         <a href="https://t.me/mitcham_il">
-          <img src={icon_telegram} alt='Icon telegram' />
+          <ReactSVG src={icon_telegram} />
         </a>
       </div>
       <span className={styles.footer__copyright}>Created 2024</span>
