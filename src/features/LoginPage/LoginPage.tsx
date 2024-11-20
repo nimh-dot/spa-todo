9146bbcea4e66d39e7dc10636d20315a328f8ac9
Vyachaslau <sokol.v083@gmail.com>
2024-11-21 01:20:06 +0300

    feat: processe login by press Enter

--- src/features/LoginPage/LoginPage.tsx
+++ src/features/LoginPage/LoginPage.tsx
@@ -1,34 +1,50 @@
 import { useRef } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import styles from './LoginPage.module.css'
 import SubmitButton from '../../components/SubmitButton/SubmitButton'
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { useAppDispatch, useAuth } from '../../app/hooks';
+import { setUser } from '../../app/userSlice'
+
 
 const LoginPage = () => {
   const emailRef = useRef(null)
   const passRef = useRef(null)
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch()
+  // const isAuth = useAuth()
 
   const handleLogin = () => {
     const auth = getAuth();
     signInWithEmailAndPassword(auth, emailRef?.current?.value, passRef?.current?.value)
       .then((userCredential) => {
-        console.log(userCredential)
-        // Signed in 
-        // const user = userCredential.user;
-        // ...
+        // console.log('successful login: ', userCredential)
+        dispatch(setUser({
+          email: userCredential?.user?.email,
+          id: userCredential?.user?.uid,
+          token: userCredential?.user?.accessToken,
+        }))
+        // console.log(isAuth)
+        navigate("/")
       })
       .catch((error) => {
         const errorCode = error.code;
         const errorMessage = error.message;
       })
-    console.log('click create account')
-    console.log(emailRef?.current?.value)
-    console.log(passRef?.current?.value)
+    // console.log('click login')
+    // console.log(emailRef?.current?.value)
+    // console.log(passRef?.current?.value)
+  }
+
+  const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      handleLogin()
+    }
   }
 
   return (
     <div className={styles.loginPage}>
-        <h1>Sing In Page</h1>
+        <h1>Login Page</h1>
         <form className={styles.form}>
           
           <label htmlFor="user">
@@ -47,7 +63,9 @@ const LoginPage = () => {
           <input 
               type="password" 
               ref={passRef}
+              id='password'
               placeholder='Enter your password'
+              onKeyDown ={(e) => {handleKeyPress(e)}}
             />
         </form>
         <SubmitButton title='Enter' handleClick={handleLogin}/>
